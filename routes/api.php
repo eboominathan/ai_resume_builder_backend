dc4ca31d369d3e15e8226aa6f79bf6d9decd89c9
Boominathan <eboominathan@gmail.com>
2024-08-01 07:43:22 +0530

    routes updated

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
-<?php 
-
+<?php
+ 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExperienceController;
 use App\Http\Controllers\EducationController;
 use App\Http\Controllers\SkillController;
-use Illuminate\Routing\Route;
 
-Route::apiResource('users', UserController::class);
+ 
+
+
+Route::apiResource('user-resumes', UserController::class);
 Route::apiResource('users.experiences', ExperienceController::class);
 Route::apiResource('users.educations', EducationController::class);
 Route::apiResource('users.skills', SkillController::class);
-
-
